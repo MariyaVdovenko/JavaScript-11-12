3e2975e34253b12a4298987674dcc2a6061aa116
MariyaVdovenko <m.vdovenko@ukr.net>
2019-04-10 03:00:22 +0300

    add homework module 03

---
+++ Module-03/js/script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+function task1() {
+  const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
+
+  const isLoginValid = function(login) {
+    if (login.length >= 4 && login.length <= 16) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  const isLoginUnique = function(AllLogins, login) {
+    if (AllLogins.includes(login)) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  const addLogin = function(allLogins, login) {
+    if (
+      isLoginValid(login) === true &&
+      isLoginUnique(allLogins, login) === true
+    ) {
+      allLogins.push(login);
+      console.log(`Логин ${login} успешно добавлен!`);
+    } else if (isLoginValid(login) === false) {
+      return console.log('Ошибка! Логин должен быть от 4 до 16 символов');
+    } else if (isLoginUnique(allLogins, login) === false) {
+      return console.log('Такой логин уже используется!');
+    }
+  };
+
+  // Вызовы функции для проверки
+  addLogin(logins, 'Ajax'); // 'Логин успешно добавлен!'
+  addLogin(logins, 'robotGoogles'); // 'Такой логин уже используется!'
+  addLogin(logins, 'Zod'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+  addLogin(logins, 'jqueryisextremelyfast'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+}
